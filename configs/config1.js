0e49c81cb89fc7771f10ea498197ae13d2ae5dcd
Hiccupscodingwhims <vedanshagarwal3052005@gmail.com>
2026-02-01 00:32:02 +0530

    changing config1

--- configs/config1.js
+++ configs/config1.js
@@ -2,14 +2,14 @@ export default {
   NAME: "config1",
 
   SCANNER: {
-    MAX_HOURS_TO_CLOSE: 4,
-    MIN_PROBABILITY: 0.80,
+    MAX_HOURS_TO_CLOSE: 2,
+    MIN_PROBABILITY: 0.85,
     MAX_PROBABILITY: 0.96,
     MIN_LIQUIDITY_USD: 10
   },
 
   TRADER: {
     TOTAL_BUDGET: 50,
-    STOP_PROB_DROP: 0.03 // 3%
+    STOP_PROB_DROP: 0.15 // 3%
   }
 };
