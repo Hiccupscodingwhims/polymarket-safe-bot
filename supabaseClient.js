6f5fc48c5d1ffac69fc0af07f9c262a5cd7d4deb
Hiccupscodingwhims <vedanshagarwal3052005@gmail.com>
2026-02-01 00:34:44 +0530

    fixed inconsistency in variable naming

--- supabaseClient.js
+++ supabaseClient.js
@@ -4,11 +4,11 @@ import { createClient } from "@supabase/supabase-js";
 if (!process.env.SUPABASE_URL) {
   throw new Error("SUPABASE_URL missing from env");
 }
-if (!process.env.SUPABASE_SERVICE_ROLE_KEY) {
-  throw new Error("SUPABASE_SERVICE_ROLE_KEY missing from env");
+if (!process.env.SUPABASE_SERVICE_KEY) {
+  throw new Error("SUPABASE_SERVICE_KEY missing from env");
 }
 
 export const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_SERVICE_KEY
 );
